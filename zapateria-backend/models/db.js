00c4fe227f7a4040a52971812b438d64bea608b8
Esthephany <echoquehuancal@ulasalle.edu.pe>
2026-07-09 19:19:10 -0500

    CONFIG BD NUBE

--- zapateria-backend/models/db.js
+++ zapateria-backend/models/db.js
@@ -1,6 +1,30 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const isProduction = process.env.DATABASE_URL;
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+      }
+);
+
+module.exports = pool;
+/*
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -8,5 +32,6 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
+*/
 
 module.exports = pool;
